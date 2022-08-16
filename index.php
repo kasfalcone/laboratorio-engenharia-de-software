46ab0c3442ee28d98b71d25ded97d65cf5df7498
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2022-08-16 20:20:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-</head>
-<body>
-    <h1>Luis henrique</h1>
-</body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Data em JavaScript</title>
+        <script type="text/javascript">
+            var data = new Date();
+            document.write(data);
+            var dia = data.getDate();
+            document.write("<br>"+dia);
+        </script>
+    </head>
+    <body>
+    </body>
 </html>
